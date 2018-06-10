8ff00d8caaed5c835c17b0ea245b2f26872e8b76
Jcarrenca <jcarrenca@retailsbs.com>
2018-06-10 13:33:47 -0400

    Initial commit

---
+++ src/main/java/cl/duoc/rifa/venta/RifaVentaApplication.java
@@ -0,0 +1,12 @@
+package cl.duoc.rifa.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RifaVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RifaVentaApplication.class, args);
+	}
+}
